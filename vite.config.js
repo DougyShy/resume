ee7874ded290ce08f095a87be8b0084a4bbdfda3
DougyShy <90754920+DougyShy@users.noreply.github.com>
2025-11-12 18:31:00 -0600

    fourth

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/resume/", // 👈 important — your GitHub repo name here
+});
